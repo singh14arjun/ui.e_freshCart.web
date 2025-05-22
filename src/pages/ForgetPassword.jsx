ce13796f2c73d7f97c66b44caa5d893a3ead1d4a
Arjun Singh <arjun2314singh@gmail.com>
2025-05-22 16:43:48 +0530

    Created Forget Password Page

---
+++ src/pages/ForgetPassword.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "../styles/forgetPassword.css";
+
+const ForgetPassword = () => {
+  return (
+    <div>
+      <div className="forget-password ">
+        <div className="forget-password-form  px-20 py-1 m-auto ">
+          <p className="font-bold text-3xl">Forget Your Password</p>
+          <p className="font-semibold text-gray-700">
+            Please enter the email address associated with your account and We
+            will email you a link to reset your password.
+          </p>
+          <form action="">
+            <input
+              type="email"
+              placeholder="Email"
+              className="border-2 p-1 rounded-xl"
+            />
+            <button className="w-full text-center bg-green-500 font-semibold text-xl p-2 cursor-pointer">
+              Reset Password
+            </button>
+
+            <button className="w-full text-center bg-gray-500 font-semibold text-xl p-2 cursor-pointer">
+              Back
+            </button>
+          </form>
+        </div>
+        <div className="forget-password-image  flex justify-center">
+          <img
+            src="https://freshcart-next-js.vercel.app/images/svg-graphics/fp-g.svg"
+            alt="loading"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ForgetPassword;
